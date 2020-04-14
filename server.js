be920414078696b0d2386742236396d287c8f168
captbilard <bilardosinulu@gmail.com>
2020-04-14 02:51:17 +0100

    Completed Story one

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const shortid = require("shortid");
 
 const cors = require('cors')
 
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
 mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+// const userSchema = new Schema({
+//   username: String,
+//   _id: {
+//     'type': String,
+//     'default': shortid.generate
+//   },
+// });
+// const User = mongoose.model('User', userSchema);
+// //find a user in the db
+// let findOneUser = function(user){
+//    User.find({username:user}, function(err, userFound){
+//     if (err) return console.log(err);
+//     if(userFound){
+//       return true
+//     }
+//     if(userFound.length == 0){
+//       return false
+//     }
+    
+//   });
+// };
+// let createAndSaveUser = function(newuser){
+//   let newlyCreatedUser = new User({username:newuser});
+//   newlyCreatedUser.save(function(err, data){
+//     if(err) return console.log(err);
+//     done(null)
+//   });
+// };
 
 app.use(cors())
 
@@ -17,7 +48,25 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+let takenUsername = [];
+let userArray = []
+//saving a new user
+app.post('/api/exercise/new-user', (req, res)=>{
+  let submittedUser = req.body.username;
+  if (takenUsername.includes(submittedUser)){
+    return res.send("username already taken")
+  }
+  let userDetails ={username:submittedUser, _id:shortid.generate()}
+  takenUsername.push(submittedUser);
+  userArray.push(userDetails);
+  return res.json(userDetails)
+});
 
+//getting all users
+app.get('api/exercise/users', (req, res)=>{
+  console.log(userArray);
+  res.json(userArray)
+})
 
 // Not found middleware
 app.use((req, res, next) => {
@@ -46,3 +95,18 @@ app.use((err, req, res, next) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+
+//saving a new user
+// app.post('/api/exercise/new-user', (req, res)=>{
+//   let submittedUser = req.body.username;
+//   User.find({name: submittedUser}, (err, arr, done)=> {
+//     if (err){
+//       console.log(err)
+//     }
+//     res.json({"checker":arr})
+//     done()
+//   });
+
+//   console.log(submittedUser)
+// });
